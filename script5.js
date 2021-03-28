51f654848351e3f4fcd9f1ae6f9003959cf39c50
GusavoCoutino <gcoutino04@gmail.com>
2021-03-27 20:05:24 -0600

    Records de las tablas incrementados

--- script5.js
+++ script5.js
@@ -1,5 +1,5 @@
 async function tabla4(){
-    const result = await fetch('https://api.airtable.com/v0/appzKexSFKJXeZPBy/Casa%20Hogar%204?maxRecords=20&view=Grid%20view'
+    const result = await fetch('https://api.airtable.com/v0/appzKexSFKJXeZPBy/Casa%20Hogar%204?maxRecords=40&view=Grid%20view'
     ,{headers:{
         "Authorization": "Bearer keyijfshbHKclSMLS"
     }})
