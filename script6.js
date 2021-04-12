c1174932983e03c2b1cba2de2a19c6928bc7a638
GusavoCoutino <gcoutino04@gmail.com>
2021-04-12 17:02:53 -0500

    Nueva institucion

---
+++ script6.js
@@ -0,0 +1,16 @@
+async function tabla5(){
+    const result = await fetch('https://api.airtable.com/v0/appzKexSFKJXeZPBy/Casa%20Hogar%205?maxRecords=60&view=Grid%20view'
+    ,{headers:{
+        "Authorization": "Bearer keyijfshbHKclSMLS"
+    }})
+    const myJson = await result.json()
+    let table5 = "<table><thead><tr><th>Producto</th><th>Estado</th></tr></thead>"
+        for(let i=0; i<myJson?.records?.length; i++){
+            console.log(myJson?.records[i])
+            table5 += "<tr><td>"+myJson?.records[i]?.fields?.Producto+"</td>"
+            table5 += "<td>"+myJson?.records[i].fields.Estado+"</td></tr>"
+        }
+        table5 += "</table>"
+        console.log(table5);
+        document.getElementById('Tabla5').innerHTML=table5
+}
